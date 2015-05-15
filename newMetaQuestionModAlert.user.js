bbe650876338cf03004c774c53cbe034308d93cf
shub <shu8@users.noreply.github.com>
2015-05-15 20:17:58 +0100

    Update newMetaQuestionModAlert.user.js
    added support for multiple sites, make it work on firefox, make it close in clicking outside of box

--- newMetaQuestionModAlert.user.js
+++ newMetaQuestionModAlert.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Meta new question alert
 // @namespace    http://stackexchange.com/users/4337810/
-// @version      1.0
+// @version      1.1
 // @description  Adds an extra mod button to the topbar to alert you of new meta questions
-// @author       ᔕᖺᘎᕊ (http://stackexchange.com/users/4337810/)
+// @author       ᔕᖺᘎᕊ (http://stackexchange.fcom/users/4337810/)
 // @match        *://*.stackexchange.com/*
 // @match        *://*.stackoverflow.com/*
 // @match        *://*.superuser.com/*
@@ -11,11 +11,13 @@
 // @match        *://*.askubuntu.com/*
 // @match        *://*.stackapps.com/*
 // @match        *://*.mathoverflow.net/*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_deleteValue
 // ==/UserScript==
-var metaName = 'meta.' + $(location).attr('hostname').split('.')[0];
+var metaName = 'meta.' + $(location).attr('hostname').split('.')[0],
+    lastQuestions = {};
 var apiLink = "https://api.stackexchange.com/2.2/questions?pagesize=5&order=desc&sort=activity&site=" + metaName;
 
 $('.topbar-links').prepend('<span id="mod-extra-icon" class="reputation links-container" style="font-size: 2em; vertical-align: top; padding-top: 3px; color: white;">♦</span>');
@@ -37,10 +39,22 @@ $('#mod-extra-icon').css('cursor', 'pointer').click(function() {
 $('#closeNewQuestionList').css('cursor', 'pointer').click(function() {
     $('#newMetaQuestionsDialog').hide();
 });
+$(document).mouseup(function (e) {
+    var container = $('#newMetaQuestionsDialog');
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        container.hide();
+    }
+});
+
+if (GM_getValue("metaNewQuestionAlert-lastQuestions", -1) == -1) {
+    GM_setValue("metaNewQuestionAlert-lastQuestions", JSON.stringify(lastQuestions));  
+} else {
+    lastQuestions = JSON.parse(GM_getValue("metaNewQuestionAlert-lastQuestions"));    
+}
 
 $.getJSON(apiLink, function(json) {
     var latestQuestion = json.items[0].title;
-    if(latestQuestion == GM_getValue("metaNewQuestionAlert-lastQuestion")) {
+    if(latestQuestion == lastQuestions[metaName]) {
         //if you've already seen the stuff
         $('#mod-extra-icon').css('color', 'white');
     } else { 
@@ -53,8 +67,9 @@ $.getJSON(apiLink, function(json) {
                 author = json.items[i].owner.display_name;
             $('#newMetaQuestionsList ul').append("<li><a href='"+link+"'>"+title+"<br>("+author+")</a>");
         }
+        lastQuestions[metaName] = latestQuestion;
         $('#mod-extra-icon').css('cursor', 'pointer').click(function() {
-            GM_setValue("metaNewQuestionAlert-lastQuestion", latestQuestion);
+            GM_setValue("metaNewQuestionAlert-lastQuestions", JSON.stringify(lastQuestions));
         });
     }
 });
