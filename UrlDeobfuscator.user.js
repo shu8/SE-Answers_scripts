b1d80ebb3ae4c2236c4a70068a3103bd7bdfc5db
shu8 <shu8@users.noreply.github.com>
2016-05-08 19:37:16 +0100

    Create UrlDeobfuscator.user.js
    http://softwarerecs.stackexchange.com/q/31534/2675

---
+++ UrlDeobfuscator.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         URL deobfuscater
+// @namespace    http://stackexchange.com/users/4337810/
+// @version      1.0
+// @description  A userscript that lets you quickly copy a cleaned-up version of the current URL
+// @author       ᔕᖺᘎᕊ (http://stackexchange.com/users/4337810/)
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.onkeyup = function(e) {
+        console.log(e.keyCode);
+        if(e.ctrlKey && e.shiftKey && (e.keyCode == 117 || e.keyCode == 85)) {
+            window.prompt('Press Ctrl+C/Ctrl+X', location.protocol + '//' + location.hostname + location.pathname);
+        }
+    };
+})();
