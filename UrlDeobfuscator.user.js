db27f3f9607889e5a575e1f6837f1bdf1557c9a3
shu8 <shu8@users.noreply.github.com>
2016-05-12 17:55:39 +0100

    #4

--- UrlDeobfuscator.user.js
+++ UrlDeobfuscator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         URL deobfuscater
 // @namespace    http://stackexchange.com/users/4337810/
-// @version      1.0
+// @version      1.0.1
 // @description  A userscript that lets you quickly copy a cleaned-up version of the current URL
 // @author       ᔕᖺᘎᕊ (http://stackexchange.com/users/4337810/)
 // @match        *://*/*
@@ -11,8 +11,25 @@
 (function() {
     'use strict';
     document.onkeyup = function(e) {
+        var x = location.href.split('?')[1].split('&'),
+            params = {},
+            extra;
+        for(var i = 0; i < x.length; i++) {
+            var s = x[i].split('=');
+            params[s[0]] = s[1];
+        }
+        //a few standard parameters that many sites use, these are useful, so keep them; https://github.com/shu8/SE-Answers_scripts/issues/4
+        if(params.s) {
+            extra = 's='+params.s;
+        } else if(params.q) {
+            extra = 'q='+params.q;
+        } else if(params.search) {
+            extra = 'search='+params.search;
+        } else if(params.query) {
+            extra = 'query='+params.query;
+        }
         if(e.ctrlKey && e.shiftKey && (e.keyCode == 117 || e.keyCode == 85)) {
-            window.prompt('Press Ctrl+C/Ctrl+X', location.protocol + '//' + location.hostname + location.pathname);
+            window.prompt('Press Ctrl+C/Ctrl+X', location.protocol + '//' + location.hostname + location.pathname + (extra ? '?' + extra : ''));
         }
     };
 })();
